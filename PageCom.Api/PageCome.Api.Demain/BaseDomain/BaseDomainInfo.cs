929f5f80ce8c813151ea58efec6877dc1475acc0
badshasha <shavendragoesoft@gmail.com>
2022-08-25 05:39:27 +0530

    [dev] add domain entity class and base entity

---
+++ PageCom.Api/PageCome.Api.Demain/BaseDomain/BaseDomainInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace PageCome.Api.Demain.BaseDomain;
+
+public class BaseDomainInfo
+{
+    public int Id { get; set; }
+    public DateTime AddDateTime { get; set; } = DateTime.Now;
+    public DateTime UpdateDateTime { get; set; } = DateTime.Now;
+}
---
+++ PageCom.Api/PageCome.Api.Demain/Book.cs
@@ -0,0 +1,10 @@
+﻿using PageCome.Api.Demain.BaseDomain;
+
+namespace PageCome.Api.Demain;
+
+public class Book : BaseDomainInfo
+{
+    public String Name { get; set; }
+    public String Description { get; set; }
+    public Double Price { get; set; }
+}
